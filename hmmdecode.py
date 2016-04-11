feb4f7eca9771bdffea62ee2d473514d6a14dd47
shivankurkapoor <shivankurkapoor3192@gmail.com>
2016-04-11 10:19:04 -0700

    Initial Commit

---
+++ hmmdecode.py
@@ -0,0 +1,247 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import math
+import json
+import time
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+class Model:
+      def __init__(self,file):
+        self.file = file
+        self.tmap = {}
+        self.emap = {}
+        self.singtt = {}
+        self.singtw = {}
+        self.ct = {}
+        self.cw = {}
+        self.ptt = {}
+        self.ptw = {}
+        self.n = 0
+        self.V = 0
+        self.pbackoffw = 0
+
+
+      def retrieveModel(self):
+          with open(self.file, 'r') as input:
+              list_maps = json.load(input)
+              self.tmap = list_maps[0]
+              self.emap = list_maps[1]
+              self.singtt = list_maps[2]
+              self.singtw = list_maps[3]
+              self.ct = list_maps[4]
+              self.cw = list_maps[5]
+              self.tagDict = list_maps[6]
+
+          for v in self.cw.itervalues():
+            self.n += v
+          self.V = len(self.cw.keys())
+
+          for t in self.ct.keys():
+              self.ptt[t] = float(self.ct[t])/float(self.n)
+          for w in self.cw.keys():
+              self.ptw[w] = float(self.cw[w] + 1)/float(self.n + self.V)
+
+
+          for k1 in self.tmap.keys():
+              for k2 in self.tmap[k1].keys():
+                  n,d = self.tmap[k1][k2].split('/')
+                  #self.tmap[k1][k2] = math.log(float(n)/float(d))
+                  self.tmap[k1][k2] = self.smoothing('T',k1,k2,n,d)
+
+          for k1 in self.emap.keys():
+              for k2 in self.emap[k1].keys():
+                  n,d = self.emap[k1][k2].split('/')
+                  #self.emap[k1][k2] = math.log(float(n)/float(d))
+                  self.emap[k1][k2] = self.smoothing('E',k1,k2,n,d)
+
+          self.pbackoffw = float(1)/float(self.n + self.V)
+
+
+      def smoothing(self,type,k1,k2,num,denom):
+          lda = 1
+          if type == 'T':
+              if k1 in self.singtt:
+                  lda += self.singtt[k1]
+              val = float(float(num) + lda*self.ptt[k2])/float(float(denom) + lda)
+
+          elif type == 'E':
+              if k1 in self.singtw:
+                  lda += self.singtw[k1]
+              val = float(float(num) + lda*self.ptw[k2])/float(float(denom) + lda)
+
+          elif type == 'MW':
+              if k1 in self.singtw:
+                  lda += self.singtw[k1]
+              val = float(float(num) + lda * self.pbackoffw)/float(float(denom) + lda)
+
+          elif type == 'MT':
+              if k1 in self.singtt:
+                  lda += self.singtt[k1]
+              val = float(float(num) + lda * self.ptt[k2])/float(float(denom) + lda)
+
+          return (val)
+
+class Decoder:
+
+     def __init__(self,sentence,model):
+        self.sentence = sentence
+        self.tmap = model.tmap
+        self.emap = model.emap
+        self.viterbi = {}
+        self.backpointer = {}
+        self.otagList = []
+        self.itagList = []
+        self.tags = []
+        self.postag = ''
+        self.model = model
+
+
+     def decode(self):
+         tokens = self.sentence.split()
+         if tokens[0] in model.tagDict:
+             self.otagList = model.tagDict[tokens[0]].keys()
+         else:
+             self.otagList = self.tmap.keys()
+             self.otagList.remove('START')
+         for k in self.otagList:
+             if k in self.tmap['START']:
+                 self.viterbi[k] = {}
+                 self.backpointer[k] = {}
+                 if tokens[0] in self.emap[k]:
+                     self.viterbi[k][0] = self.tmap['START'][k] * self.emap[k][tokens[0]]
+                 else:
+                     self.viterbi[k][0] = self.tmap['START'][k] * model.smoothing('MW',k,tokens[0],0,model.ct[k])
+
+                 self.backpointer[k] = {}
+                 self.backpointer[k][0] = 'START'
+
+         for i in range(1,len(tokens)):
+             if tokens[i] in model.tagDict:
+                 self.otagList = model.tagDict[tokens[i]].keys()
+             else:
+                 self.otagList = self.tmap.keys()
+                 self.otagList.remove('START')
+             for k1 in self.otagList:
+
+                 maxVal = 0
+                 if tokens[i-1] in model.tagDict:
+                     self.itagList = model.tagDict[tokens[i-1]].keys()
+                 else:
+                     self.itagList = self.tmap.keys()
+                     self.itagList.remove('START')
+                 for k2 in self.itagList:
+                     if k1 in self.tmap[k2]:
+                        if tokens[i] in self.emap[k1] and k2 in self.viterbi and i-1 in self.viterbi[k2]:
+                           maxVal = max(maxVal,self.viterbi[k2][i-1]*self.tmap[k2][k1]*self.emap[k1][tokens[i]])
+                        elif k2 in self.viterbi and i-1 in self.viterbi[k2]:
+                           maxVal = max(maxVal,self.viterbi[k2][i-1] * self.tmap[k2][k1] * model.smoothing('MW',k1,tokens[i],0,model.ct[k1]))
+
+                     else:
+                         if tokens[i] in self.emap[k1] and k2 in self.viterbi and i-1 in self.viterbi[k2]:
+                           maxVal = max(maxVal,self.viterbi[k2][i-1]*model.smoothing('MT',k2,k1,0,model.ct[k2])*self.emap[k1][tokens[i]])
+                         elif k2 in self.viterbi and i-1 in self.viterbi[k2]:
+                           maxVal = max(maxVal,self.viterbi[k2][i-1] * model.smoothing('MT',k2,k1,0,model.ct[k2]) * model.smoothing('MW',k1,tokens[i],0,model.ct[k1]))
+
+
+                 if k1 in self.viterbi:
+                   self.viterbi[k1][i] = maxVal
+                 else:
+                   self.viterbi[k1] = {}
+                   self.viterbi[k1][i] = maxVal
+
+
+                 maxVal = 0
+                 argMax = ''
+                 for k2 in self.itagList:
+                    if k1 in self.tmap[k2]:
+                        if k2 in self.viterbi and i-1 in self.viterbi[k2]:
+                           curr_val = self.viterbi[k2][i-1]*self.tmap[k2][k1]
+                           if maxVal <= curr_val:
+                              maxVal = curr_val
+                              argMax = k2
+                    else:
+                        if k2 in self.viterbi and i-1 in self.viterbi[k2]:
+                           curr_val = self.viterbi[k2][i-1]*model.smoothing('MT',k2,k1,0,model.ct[k2])
+                           if maxVal <= curr_val:
+                              maxVal = curr_val
+                              argMax = k2
+
+
+                 if k1 in self.backpointer:
+                    self.backpointer[k1][i] = argMax
+                 else:
+                    self.backpointer[k1] = {}
+                    self.backpointer[k1][i] = argMax
+
+
+         maxVal = 0
+         argMax = ''
+         if tokens[len(tokens)-1] in model.tagDict:
+             self.otagList = model.tagDict[tokens[len(tokens)-1]].keys()
+         else:
+             self.otagList = self.tmap.keys()
+             self.otagList.remove('START')
+         for k1 in self.otagList:
+             if 'END' in self.tmap[k1]:
+                 curr_val = self.viterbi[k1][len(tokens)-1]*self.tmap[k1]['END']
+                 if maxVal <= curr_val:
+                      maxVal = curr_val
+                      argMax = k1
+             else:
+                 curr_val = self.viterbi[k1][len(tokens)-1]*model.smoothing('MT',k1,'END',0,model.ct[k1])
+                 if maxVal <= curr_val:
+                      maxVal = curr_val
+                      argMax = k1
+
+
+         self.viterbi['END'] = {}
+         self.backpointer['END'] = {}
+         self.viterbi['END'][len(tokens)-1] = maxVal
+         self.backpointer['END'][len(tokens)-1] = argMax
+
+
+     def backtrack(self):
+         tokens = self.sentence.split()
+         tag = (self.backpointer['END'][len(tokens)-1])
+         self.tags.append(tag)
+
+         prevTag = tag
+         for i in range(len(tokens)-1, -1, -1):
+            currTag = self.backpointer[prevTag][i]
+            self.tags.append(currTag)
+            prevTag = currTag
+
+         self.tags.reverse()
+
+     def POSTag(self):
+
+         tokens = self.sentence.split()
+         for i in range(len(tokens)):
+             self.postag = self.postag + tokens[i] +'/'+ self.tags[i+1] + ' '
+
+
+if __name__=="__main__":
+   start = time.clock()
+   model_data = 'hmmmodel.txt'
+   model = Model(model_data)
+   model.retrieveModel()
+
+   result = []
+  # print sys.argv[1]
+
+   with open(sys.argv[1], 'r') as input:
+       for line in input:
+        decoder = Decoder(line,model)
+        decoder.decode()
+        decoder.backtrack()
+        decoder.POSTag()
+        result.append(decoder.postag)
+
+   with open('hmmoutput.txt','w') as f:
+       for r in result:
+           f.write(r+"\n")
+   f.close()
+   print time.clock()-start
---
+++ hmmlearn.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+
+import re
+import sys
+import codecs
+import json
+import time
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+class SmoothingModel:
+
+    def __init__(self,tmap,emap,sentence_list):
+        self.tmap = tmap
+        self.emap = emap
+        self.list = sentence_list
+        self.singtt = {}
+        self.singtw = {}
+        self.ct = {}
+        self.cw = {}
+        self.tagDict = {}
+
+    def genSingTT(self):
+        for k1 in self.tmap.keys():
+            count = 0
+            for k2 in self.tmap[k1].keys():
+                if self.tmap[k1][k2] == 1:
+                    count += 1
+            self.singtt[k1] = count
+
+    def genSingTW(self):
+        for k1 in self.emap.keys():
+            count = 0
+            for k2 in self.emap[k1].keys():
+                if self.emap[k1][k2] == 1:
+                    count += 1
+            self.singtw[k1] = count
+
+    def backOff(self):
+        for sentence in self.list:
+            tokens = sentence.split()
+            for i in range(len(tokens)):
+                word = tokens[i].rsplit('/',1)[0]
+                tag = tokens[i].rsplit('/',1)[1]
+                if tag in self.ct:
+                    self.ct[tag] += 1
+                else:
+                    self.ct[tag] = 1
+                if word in self.cw:
+                    self.cw[word] += 1
+                else:
+                    self.cw[word] = 1
+                if word in self.tagDict:
+                    self.tagDict[word][tag] = 1
+                else:
+                    self.tagDict[word] = {}
+                    self.tagDict[word][tag] = 1
+        self.ct['END'] = len(self.list)
+
+class HMMModel:
+
+    def __init__(self,sentence_list):
+        self.list = sentence_list
+        self.tmap = {}
+        self.emap = {}
+
+    def train(self):
+        for sentence in sentence_list:
+            tokens = sentence.split()
+            for i in range(len(tokens)-1):
+                word = tokens[i].rsplit('/',1)[0]
+                curr_tag = tokens[i].rsplit('/',1)[1]
+                next_tag = tokens[i+1].rsplit('/',1)[1]
+
+                #filling transition matrix
+                if i == 0:
+                    if 'START' in self.tmap:
+                        if curr_tag in self.tmap['START']:
+                            self.tmap['START'][curr_tag] += 1
+                        else:
+                           self.tmap['START'][curr_tag] = 1
+                    else:
+                        self.tmap['START'] = {}
+                        self.tmap['START'][curr_tag] = 1
+
+                if curr_tag in self.tmap:
+                    if next_tag in self.tmap[curr_tag]:
+                        self.tmap[curr_tag][next_tag] += 1
+                    else:
+                        self.tmap[curr_tag][next_tag] = 1
+                else:
+                    self.tmap[curr_tag] = {}
+                    self.tmap[curr_tag][next_tag] = 1
+
+                #filling emmison matrix
+                if curr_tag in self.emap:
+                    if word in self.emap[curr_tag]:
+                        self.emap[curr_tag][word] += 1
+                    else:
+                        self.emap[curr_tag][word] = 1
+                else:
+                    self.emap[curr_tag] = {}
+                    self.emap[curr_tag][word] = 1
+
+                if i == len(tokens)-2:
+                    last_word = tokens[i+1].rsplit('/',1)[0]
+                    last_tag = next_tag
+
+                    if last_tag in self.tmap:
+                        if 'END' in self.tmap[last_tag]:
+                            self.tmap[last_tag]['END'] += 1
+                        else:
+                            self.tmap[last_tag]['END'] = 1
+                    else:
+                        self.tmap[last_tag] = {}
+                        self.tmap[last_tag]['END'] = 1
+
+                    if last_tag in self.emap:
+                        if last_word in self.emap[last_tag]:
+                            self.emap[last_tag][last_word] += 1
+                        else:
+                            self.emap[last_tag][last_word] = 1
+                    else:
+                        self.emap[last_tag] = {}
+                        self.emap[last_tag][last_word] = 1
+
+
+if __name__=="__main__":
+   start = time.clock()
+   training_data = sys.argv[1]
+   sentence_list = []
+   with open(training_data, 'r') as input:
+       for line in input:
+           sentence_list.append(line)
+   model = HMMModel(sentence_list)
+   model.train()
+   smoothingmodel = SmoothingModel(model.tmap,model.emap,sentence_list)
+   smoothingmodel.genSingTT()
+   smoothingmodel.genSingTW()
+   smoothingmodel.backOff()
+
+   for k1,v1 in model.tmap.items():
+        count = 0
+        for k2,v2 in v1.items():
+            count += v2
+        for k2,v2 in v1.items():
+            v1[k2] = str(str(v2)+"/"+str(count))
+
+   for k1,v1 in model.emap.items():
+        count = 0
+        for k2,v2 in v1.items():
+            count += v2
+        for k2,v2 in v1.items():
+            v1[k2] = str(str(v2)+"/"+str(count))
+
+   list_maps = []
+   list_maps.append(model.tmap)
+   list_maps.append(model.emap)
+   list_maps.append(smoothingmodel.singtt)
+   list_maps.append(smoothingmodel.singtw)
+   list_maps.append(smoothingmodel.ct)
+   list_maps.append(smoothingmodel.cw)
+   list_maps.append(smoothingmodel.tagDict)
+
+   with open('hmmmodel.txt', 'w') as f:
+       json.dump(list_maps,f)
+   f.close()
